0af0eaa2114a5161dcde64f06f8551c1881b5f15
Aidan <aidan.rypens@outlook.com>
2019-02-06 23:40:30 +0100

    Updated build

---
+++ Frontend/src/scenes/account/Login.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+interface IState {}
+
+interface IProps {}
+
+class Login extends Component<IProps, IState> {
+  render() {
+    return <div />;
+  }
+}
+
+export default Login;
---
+++ Frontend/src/scenes/account/Register.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+interface IState {}
+
+interface IProps {}
+
+class Register extends Component<IProps, IState> {
+  render() {
+    return <div />;
+  }
+}
+
+export default Register;
