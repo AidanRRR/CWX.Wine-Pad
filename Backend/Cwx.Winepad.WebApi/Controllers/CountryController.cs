b14cd4bde97f5e3c110c188abcc5a1efde65ad0a
Tars Hoeykens <tarshoeykens@gmail.com>
2019-02-26 00:01:27 +0100

    beetje comments, TE DOEN  proberen eentje met id op te halen

--- Backend/Cwx.Winepad.WebApi/Controllers/CountryController.cs
+++ Backend/Cwx.Winepad.WebApi/Controllers/CountryController.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Cwx.Winepad.Domain.Models;
 
 namespace Cwx.Winepad.WebApi.Controllers
 {
@@ -15,12 +16,18 @@ namespace Cwx.Winepad.WebApi.Controllers
         }
 
         [HttpPost("country")]
+        //FromBody bind de data 
         public Task AddCountry([FromBody] AddCountry.Request request) => _mediator.Send(request);
 
         [HttpPut("country")]
+                                    // FromBody Haalt  de data uit de request body van de http request (na de header) --> Osi model
         public Task UpdateCountry([FromBody] UpdateCountry.Request request) => _mediator.Send(request);
 
         [HttpGet("country")]
+                                                //FromQuery haalt de parameters uit de URL
         public Task<GetCountry.Response> GetCountry([FromQuery] GetCountry.Request request) => _mediator.Send(request);
-    }
-}
+//
+//        [HttpGet("country") //[FromUri] id moet  je niet definieeren,  is by default. De rest is wel uit body request, wel definieeren
+//        public Task<GetCountry.Response> GetCountryWithId(int id,[FromBody] GetCountry.Request request) => _mediator.Send(request);
+//    }
+//}
